4c0af5de96fb71532e53475cbe4bdfcd0691bd2f
pooja-narula <poojanarula.nsit@gmail.com>
2025-08-25 19:04:03 +0000

    added all test cases

--- src/kata.py
+++ src/kata.py
@@ -1,24 +1,40 @@
 import re
-def extract_num(s):
-    r=re.findall('[\\d]+(?:[,]\\d+)?(?:[.]\\d+)?',s)
-    num="".join(r).split(",")
-    return "".join(num) if len(r) else 0
-def add_num(inp) :
-    delimProvided=re.findall('//(?:[^\\n]*)\n',inp)
-    # step 4, making delim optional, thereby satisfying all previous steps
-    delim=delimProvided[0][2:-1] if len(delimProvided) and len(delimProvided[0])>3 else ","
-    start=len(delimProvided[0]) if len(delimProvided) else 0
-    inp=inp[start:]
-    #step 3, treating \n as delim 
-    inp=inp.replace('\n',delim)
-    #step 5, throw an exception “negatives not allowed”
-    if delim!='-' and len(inp.split("-"))>1:
-        r=list(map(lambda v:v.split(delim)[0],inp.split("-")[1:]))
-        raise Exception('negatives not allowed: -'+ ",-".join(r))
-    numList=inp.split(delim)
-    result = list(map(lambda s: float(extract_num(s.strip())), numList))
-    s=sum(result)
-    return int(s) if int(s)==s else s
+
+class Kata:
+    def extract_num(self, s):
+        r=re.findall('[\\d]+(?:[,]\\d+)?(?:[.]\\d+)?',s)
+        num="".join(r).split(",")
+        return "".join(num) if len(r) else 0
+    
+    def extract_delim(self, s):
+        delims=[]
+        delimProvided=re.findall('//(?:[^\\n]*)\n',s)
+        # step 4, making delim optional, thereby satisfying all previous steps
+        delim=delimProvided[0][2:-1] if len(delimProvided) and len(delimProvided[0])>3 else ","
+        # step 8, multiple delimiters [*][+]
+        delims=re.findall(r'\[(?:[^\]]*)\]',delim)
+        delims=list(map(lambda v: v[1:-1], delims))
+        self.delimInpLength = len(delimProvided[0]) if len(delimProvided) else 0
+        return delims if len(delims) else [delim] 
+    
+    def add_num(self, inp) :
+        self.inp=inp
+        delims = self.extract_delim(inp)
+        start = self.delimInpLength
+        inp=inp[start:]
+        #step 3, treating \n as delim 
+        inp=inp.replace('\n',delims[0])
+        numList=re.split("["+"".join(delims)+"]",inp)
+        #step 5, throw an exception “negatives not allowed”
+        if '-' not in delims and len(inp.split("-"))>1:
+            negNums=[]
+            for numString in numList:
+                if '-' in numString:
+                    negNums.append(numString.strip().split('-')[1])
+            raise Exception('negatives not allowed: -'+ ",-".join(negNums))
+        result = list(map(lambda s: float(self.extract_num(s.strip())), numList))
+        s=sum(result)
+        return int(s) if int(s)==s else s
 
 if __name__=="__main__":
     inp=input()
--- test/kata.py
+++ test/kata.py
@@ -3,11 +3,23 @@ import sys
 sys.path.append('.')
 sys.path.append('..')
 from src import kata
-
+kata=kata.Kata()
+OKCYAN= '\033[96m'
+OKGREEN= '\033[92m'
+BOLD='\033[1m'
+ENDC='\033[0m'
 class KataTest(unittest.TestCase):
+    def prettyPrint(self, testResult):
+        fname=self._testMethodName.split("_")
+        test_fname="".join(fname[1:2])
+        test_case="_".join(fname[2:])
+        print(f"[{test_fname}] {OKCYAN}[{test_case}]{ENDC}  Input: {OKGREEN}{BOLD}{repr(self.inp)}{ENDC} {testResult}")
     def assertEqualVerbose(self, expected, actual):
-        print(f"{self._testMethodName} Input:{repr(self.inp)} Asserting: {expected} == {actual}")
+        self.prettyPrint(f'Asserting: {expected} == {actual}')
         self.assertEqual(expected, actual)
+    def assertRaisesRegexVerbose(self, exception, regex, fn, arg):
+        self.prettyPrint(f'Asserting exception == {regex}')
+        self.assertRaisesRegex(exception, regex, fn, arg)
     def test_add_empty(self):
         self.inp=""
         self.assertEqualVerbose(0, kata.add_num(self.inp))
@@ -15,8 +27,8 @@ class KataTest(unittest.TestCase):
         self.inp="3,2.2"
         self.assertEqualVerbose(5.2, kata.add_num(self.inp))
     def test_add_negative(self):
-        self.inp="3,-2 "
-        self.assertRaisesRegex(Exception, "negatives not allowed: -2", kata.add_num, self.inp)
+        self.inp="3,-2,-7 "
+        self.assertRaisesRegexVerbose(Exception, "negatives not allowed: -2,-7", kata.add_num, self.inp)
     def test_add_default_delim(self):
         self.inp="3,2"
         self.assertEqualVerbose(5, kata.add_num(self.inp))
@@ -26,6 +38,12 @@ class KataTest(unittest.TestCase):
     def test_add_delim(self):
         self.inp="//+\n3+2"
         self.assertEqualVerbose(5, kata.add_num(self.inp))
+    def test_add_delim_anylength(self):
+        self.inp="//+++\n3+++2"
+        self.assertEqualVerbose(5, kata.add_num(self.inp))
+    def test_add_multiple_delim(self):
+        self.inp="//[++][*]\n3++2*5"
+        self.assertEqualVerbose(10, kata.add_num(self.inp))
     def test_add_special_delim(self):
         spcl=['.','-']
         for s in spcl:
