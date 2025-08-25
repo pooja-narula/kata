9f48604488385713c59632043647a21f17be88af
pooja-narula <poojanarula.nsit@gmail.com>
2025-08-25 12:53:11 +0000

    fix for tdd style

--- test/kata.py
+++ test/kata.py
@@ -6,24 +6,36 @@ from src import kata
 
 class KataTest(unittest.TestCase):
     def assertEqualVerbose(self, expected, actual):
-        print(f"Asserting: {expected} == {actual}")
+        print(f"{self._testMethodName} Input:{repr(self.inp)} Asserting: {expected} == {actual}")
         self.assertEqual(expected, actual)
-    def test_add(self):
-        inp=" cds , od"
-        self.assertEqualVerbose(0, kata.add_num(inp))
-        inp=" 3 , 2.2 "
-        self.assertEqualVerbose(5.2, kata.add_num(inp))
-        inp=" 3 , 2 "
-        self.assertEqualVerbose(5, kata.add_num(inp))
-        inp=" 3000, 2 "
-        self.assertEqualVerbose(3002, kata.add_num(inp))
-        inp="//+\n 3,00000+ 0.2 "
-        self.assertEqualVerbose(300000.2, kata.add_num(inp))
-        inp="//+\n 3,000 + 20,700.20 "
-        self.assertEqualVerbose(23700.2, kata.add_num(inp))
-        inp="//+\n 3000 \n -2020 "
-        self.assertRaisesRegex(Exception, "negatives not allowed: -2020",
-                       kata.add_num, inp)
-
+    def test_add_empty(self):
+        self.inp=""
+        self.assertEqualVerbose(0, kata.add_num(self.inp))
+    def test_add_float(self):
+        self.inp="3,2.2"
+        self.assertEqualVerbose(5.2, kata.add_num(self.inp))
+    def test_add_negative(self):
+        self.inp="3,-2 "
+        self.assertRaisesRegex(Exception, "negatives not allowed: -2", kata.add_num, self.inp)
+    def test_add_default_delim(self):
+        self.inp="3,2"
+        self.assertEqualVerbose(5, kata.add_num(self.inp))
+    def test_add_optional_delim(self):
+        self.inp="1\n3,2"
+        self.assertEqualVerbose(6, kata.add_num(self.inp))
+    def test_add_delim(self):
+        self.inp="//+\n3+2"
+        self.assertEqualVerbose(5, kata.add_num(self.inp))
+    def test_add_special_delim(self):
+        spcl=['.','-']
+        for s in spcl:
+            self.inp="//"+s+"\n3"+s+"2"+s+"1"
+            self.assertEqualVerbose(6, kata.add_num(self.inp))
+    def test_add_input_extractor_comma(self):
+        self.inp="//+\n3,000+2,000"
+        self.assertEqualVerbose(5000, kata.add_num(self.inp))
+    def test_add_input_extractor_text(self):
+        self.inp="3km , 2km, 1.5km"
+        self.assertEqualVerbose(6.5, kata.add_num(self.inp))
 if __name__=="__main__":
     unittest.main()
